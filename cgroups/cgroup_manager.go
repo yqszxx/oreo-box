4708822fc85d6321b627ab68a5fbef5ab12508c6
dk-kristy <DK_Kristy@outlook.com>
2020-04-23 22:17:05 +0800

    Handle errors in cgroups(one left)

--- cgroups/cgroup_manager.go
+++ cgroups/cgroup_manager.go
@@ -1,7 +1,7 @@
 package cgroups
 
 import (
-	"github.com/Sirupsen/logrus"
+	"fmt"
 	"github.com/yqszxx/oreo-box/cgroups/subsystems"
 )
 
@@ -18,14 +18,18 @@ func NewCgroupManager(path string) *CgroupManager {
 
 func (c *CgroupManager) Apply(pid int) error {
 	for _, subSysIns := range subsystems.SubsystemsIns {
-		subSysIns.Apply(c.Path, pid)
+		if err := subSysIns.Apply(c.Path, pid); err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 func (c *CgroupManager) Set(res *subsystems.ResourceConfig) error {
 	for _, subSysIns := range subsystems.SubsystemsIns {
-		subSysIns.Set(c.Path, res)
+		if err := subSysIns.Set(c.Path, res); err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -33,7 +37,7 @@ func (c *CgroupManager) Set(res *subsystems.ResourceConfig) error {
 func (c *CgroupManager) Destroy() error {
 	for _, subSysIns := range subsystems.SubsystemsIns {
 		if err := subSysIns.Remove(c.Path); err != nil {
-			logrus.Warnf("remove cgroup fail %v", err)
+			return fmt.Errorf("remove cgroup fail %v", err)
 		}
 	}
 	return nil
--- cgroups/subsystems/utils.go
+++ cgroups/subsystems/utils.go
@@ -38,11 +38,11 @@ func GetCgroupPath(subsystem string, cgroupPath string, autoCreate bool) (string
 		if os.IsNotExist(err) {
 			if err := os.Mkdir(path.Join(cgroupRoot, cgroupPath), 0755); err == nil {
 			} else {
-				return "", fmt.Errorf("error create cgroup %v", err)
+				return "", fmt.Errorf("cannot create cgroup %v", err)
 			}
 		}
 		return path.Join(cgroupRoot, cgroupPath), nil
 	} else {
-		return "", fmt.Errorf("cgroup path error %v", err)
+		return "", fmt.Errorf("cgroup path error: %v", err)
 	}
 }
